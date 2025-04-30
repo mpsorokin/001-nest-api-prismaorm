980b804ee561ad615cdb566ed9016f393bad6b3d
Aa <aa@example.com>
2025-04-30 15:45:57 +0300

    cleaned uo

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,19 @@ import { ConfigModule } from '@nestjs/config';
 import { MovieModule } from './movie/movie.module';
 import { ReviewModule } from './review/review.module';
 import { ActorModule } from './actor/actor.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
     TaskModule,
     MovieModule,
     ReviewModule,
     ActorModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
