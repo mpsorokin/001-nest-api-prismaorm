827dae395908bbcd0a7623b3cb1da1130943218d
Aa <aa@example.com>
2025-05-01 17:13:06 +0300

    at least works

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,17 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest js docs')
+    .setDescription('API Documentation nest js docs')
+    .setVersion('1.0.0')
+    .setContact('name', 'http://localhost:8080/url', 'someemail@test.com')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(process.env.PORT ?? 3070);
 }
--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -6,24 +6,60 @@ import {
   Param,
   Post,
   Put,
+  HttpStatus,
 } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { MovieDto } from './dto/movie.dto';
+import {
+  ApiBody,
+  ApiHeader,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Movies')
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
+  @ApiOperation({ summary: 'Get movies', description: 'Get movies' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Success' })
   @Get()
   findAll() {
     return this.movieService.findAll();
   }
 
+  @ApiOperation({ summary: 'Create movie', description: 'Create movie' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        title: { type: 'string', example: 'Movie Title' },
+      },
+    },
+  })
   @Post()
   create(@Body() dto: MovieDto) {
     return this.movieService.create(dto);
   }
 
+  @ApiOperation({
+    summary: 'Get movie by id',
+    description: 'Get movie by id',
+  })
+  //@ApiParam({ name: 'id', type: 'string', description: 'The id of the movie' })
+  /*@ApiQuery({
+    name: 'year',
+    type: 'string',
+    description: 'The year of the movie',
+    required: false,
+  })*/
+  @ApiHeader({ name: 'Authorization', description: 'Auth token' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Success' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Not Found' })
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.movieService.findById(id);
